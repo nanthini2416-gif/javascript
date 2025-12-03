0999b2d8ee9679228529899b0cc62a9c755df64d
nanthini2416-gif <nanthini2416@gmail.com>
2025-12-03 09:53:38 +0530

    task

---
+++ ex.js
@@ -0,0 +1,5 @@
+alert("hello World")
+console.log("I am inside console window")
+
+document.body.innerHTML = "<h1>This is From External File"
+document.body.innerHTML += "<h1>This is From External File"
